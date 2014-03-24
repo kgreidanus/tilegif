6725c18a2522e0c6be6f214ba3e4dfff89165fde
Kevin G <kgreidanus@strathcom.com>
2014-03-23 23:33:47 -0600

    Holy fucking demo mode.

--- tilelib.js
+++ tilelib.js
@@ -1,3 +1,5 @@
+
+
 function random_gif() {
   gif_index = Math.floor(Math.random()*GIF_URLS.length);
   setListGif();
@@ -36,6 +38,29 @@ function setManualGif() {
   gifarea.value = "";
 }
 
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+      // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+      // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+      // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  } 
+    return query_string;
+}();
+
 function setManualOverlay() {
   url = overlayarea.value;
   
@@ -48,9 +73,13 @@ function setManualOverlay() {
 }
 
 function setListGif() {
+
   gif_index_max = GIF_URLS.length-1;
 
   gif_index_display.innerHTML = gif_index + 1;
+  if(remote_control){
+    win2.gif_index_display.innerHTML = gif_index + 1;
+  }
   gif_index_max_display.innerHTML = gif_index_max + 1;
   setBackgroundImageFromUrl(rawr, GIF_URLS[gif_index]);
 }
